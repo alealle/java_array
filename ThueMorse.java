202233450d9f7e377eb6364c7f2a5ce295135fdb
alealle <106605527+alealle@users.noreply.github.com>
2024-02-20 20:42:11 -0300

    Print ThueMorse matrix as indicated in question. Working with messages at each sub sequence until complete n bits

--- ThueMorse.java
+++ ThueMorse.java
@@ -1,6 +1,6 @@
 public class ThueMorse {
   public static void main(String[] args) {
-    if (args.length < 1) {
+    if (args.length != 1) {
       System.out.println("Provide at least 1 argument: the size n of the nxn pattern");
     } else {
       int n = Integer.parseInt(args[0]);
@@ -21,6 +21,18 @@ public class ThueMorse {
         System.out.println("Size last seq = " + sizeLastSequence);
         System.out.println(out);
       }
-    }
+
+      // Use sequence to print matrix
+      for (i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (thueMorseSequence[i] == thueMorseSequence[j]) {
+            System.out.print("+ ");
+          } else {
+            System.out.print("- ");
+          }
+        }
+        System.out.println();
+      }
+    } // else condition one argumento to program
   }
 }
