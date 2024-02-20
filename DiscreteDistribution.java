46444b7d207070600e6d0d5074064c7aa7167ad3
alealle <106605527+alealle@users.noreply.github.com>
2024-02-20 17:51:33 -0300

    Changed styles warnings from grader. Implemented (int) instead of (round) to overcome randomness

--- DiscreteDistribution.java
+++ DiscreteDistribution.java
@@ -7,46 +7,48 @@ public class DiscreteDistribution {
 
     } else {
       int m = Integer.parseInt(args[0]);
-      int a[] = new int[n];
+      int[] a = new int[n];
 
       for (int i = 0; i < n; i++) {
         a[i] = Integer.parseInt(args[i + 1]);
       }
 
       // check input
-      for (int i = 0; i < n; i++) {
-        System.out.println(a[i]);
-      }
-
+      /*
+       * for (int i = 0; i < n; i++) {
+       * System.out.println(a[i]);
+       * }
+       */
       // generate cumulative sum
-      int s[] = new int[n];
+      int[] s = new int[n];
       s[0] = a[0];
       for (int i = 1; i < n; i++) {
         s[i] = s[i - 1] + a[i];
       }
       // check cumulative sum
-      String outS = "";
-      for (int i = 0; i < n; i++) {
-        outS += s[i] + " ";
-      }
-      System.out.println(outS);
+      /*
+       * String outS = "";
+       * for (int i = 0; i < n; i++) {
+       * outS += s[i] + " ";
+       * }
+       * System.out.println(outS);
+       */
+      int totalSum = s[n - 1];
 
-      int total_sum = s[n - 1];
-
-      System.out.println("total: " + total_sum);
+      // System.out.println("total: " + totalSum);
       // generate output
-      String out = "";
+      StringBuilder out = new StringBuilder();
       for (int i = 0; i < m; i++) {
-        int prob_to_index = Math.round(((float) Math.random()) * total_sum);
-        System.out.println("prob_to_index = " + prob_to_index);
+        long probToIndex = (long) ((Math.random()) * totalSum);
+        // System.out.println("prob_to_index = " + prob_to_index);
         int j = 0;
-        while (prob_to_index > s[j]) {
-          System.out.format(
-              "probability_to_idx: %d - i: %d - s[%d]: %d%n", prob_to_index, j, j, s[j]);
+        while (probToIndex > s[j]) {
+          // System.out.format(
+          // "probability_to_idx: %d - i: %d - s[%d]: %d%n", prob_to_index, j, j, s[j]);
           j++;
         }
         int index = j + 1;
-        out += index + " ";
+        out.append(index + " ");
       }
       System.out.println(out);
     }
